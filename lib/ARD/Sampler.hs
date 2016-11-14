6388b72220ab5e643d1b65d2f86d9a6bcc5d6786
Michael Zoech <michi.zoech@gmail.com>
2016-11-13 16:54:38 -0800

    Hemisphere samples generation with hardcoded e (power)

--- lib/ARD/Sampler.hs
+++ lib/ARD/Sampler.hs
@@ -10,6 +10,7 @@ data Sampler
   = Sampler
   { numSamples :: Int
   , unitSquareSamples :: [Point2]
+  , hemisphereSamples :: [Vector3]
   } deriving (Eq, Show)
 
 mkRegular :: Int -> Sampler
@@ -19,8 +20,25 @@ mkRegular samplesPerAxis =
     points = map (/n) [0.5..n-1]
     samples = [ Vector2 x y | y <- points, x <- points ]
   in
-    Sampler (samplesPerAxis*samplesPerAxis) samples
+    Sampler (samplesPerAxis*samplesPerAxis) samples (map (unitSquareSampleToHemisphereSample 2) samples)
 
 mkStandard :: Sampler
-mkStandard = Sampler 1 [Vector2 0.5  0.5]
+mkStandard =
+  let
+    samples = [Vector2 0.5 0.5]
+  in
+    Sampler 1 samples (map (unitSquareSampleToHemisphereSample 2) samples)
+
+unitSquareSampleToHemisphereSample :: Double -> Point2 -> Vector3
+unitSquareSampleToHemisphereSample e (Vector2 x y) =
+  let
+    cosphi = cos (2 * pi * x)
+    sinphi = sin (2 * pi * x)
+    costheta = (1 - y) ** (1 / (e+1))
+    sintheta = sqrt (1 - costheta * costheta)
+    pu = sintheta * cosphi
+    pv = sintheta * sinphi
+    pw = costheta
+  in
+    Vector3 pu pv pw
 
