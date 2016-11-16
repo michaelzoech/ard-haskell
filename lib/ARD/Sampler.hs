daea641ea137c128c0680cd7c6a6a5cfce8a168f
Michael Zoech <michi.zoech@gmail.com>
2016-11-16 14:29:25 -0800

    Add random and jittered sampler

--- lib/ARD/Sampler.hs
+++ lib/ARD/Sampler.hs
@@ -1,5 +1,7 @@
 module ARD.Sampler
   ( Sampler(..)
+  , mkJittered
+  , mkRandom
   , mkRegular
   , mkStandard
   ) where
@@ -13,11 +15,39 @@ data Sampler
   , hemisphereSamples :: [Vector3]
   } deriving (Eq, Show)
 
+mkJittered :: Int -> [Double] -> Sampler
+mkJittered samplesPerAxis randoms =
+  let
+    n = fromIntegral samplesPerAxis
+    invN = 1 / n
+    points = [0..n-1]
+    unscaledSamples = [ (x,y) | y <- points, x <- points ]
+    randomPairs = mapAdjacent (,) randoms
+    samples = map (\((x,y),(a,b)) -> Vector2 ((x+a)*invN) ((y+b)*invN) ) $ zip unscaledSamples randomPairs
+  in
+    Sampler
+      { numSamples = samplesPerAxis * samplesPerAxis
+      , unitSquareSamples = samples
+      , hemisphereSamples = map (unitSquareSampleToHemisphereSample 2) samples
+      }
+
+mkRandom :: Int -> [Double] -> Sampler
+mkRandom numSamples randoms =
+  let
+    samples = take numSamples $ mapAdjacent Vector2 randoms
+  in
+    Sampler
+      { numSamples = numSamples
+      , unitSquareSamples = samples
+      , hemisphereSamples = map (unitSquareSampleToHemisphereSample 2) samples
+      }
+
 mkRegular :: Int -> Sampler
 mkRegular samplesPerAxis =
   let
     n = fromIntegral samplesPerAxis
-    points = map (/n) [0.5..n-1]
+    invN = 1 / n
+    points = map (*invN) [0.5..n-1]
     samples = [ Vector2 x y | y <- points, x <- points ]
   in
     Sampler (samplesPerAxis*samplesPerAxis) samples (map (unitSquareSampleToHemisphereSample 2) samples)
@@ -28,6 +58,10 @@ mkStandard =
     samples = [Vector2 0.5 0.5]
   in
     Sampler 1 samples (map (unitSquareSampleToHemisphereSample 2) samples)
+
+mapAdjacent :: (a -> a -> b) -> [a] -> [b]
+mapAdjacent f (x:y:zs) = f x y : mapAdjacent f zs
+mapAdjacent _ _ = []
 
 unitSquareSampleToHemisphereSample :: Double -> Point2 -> Vector3
 unitSquareSampleToHemisphereSample e (Vector2 x y) =
--- test/ARD/SamplerSpec.hs
+++ test/ARD/SamplerSpec.hs
@@ -7,16 +7,40 @@ import Test.Hspec
 
 spec :: Spec
 spec = describe "Sampler" $ do
+  jitteredSamplerSpec
   standardSamplerSpec
+  randomSamplerSpec
   regularSamplerSpec
 
+jitteredSamplerSpec :: Spec
+jitteredSamplerSpec = describe "JitteredSampler" $ do
+  it "with non-random values matches RegularSampler" $
+    mkJittered 2 [0.5,0.5..] `shouldBe` mkRegular 2
+  it "uses given list to jitter points" $ do
+    unitSquareSamples (mkJittered 2 [0,0..]) `shouldBe` [Vector2 0 0, Vector2 0.5 0, Vector2 0 0.5, Vector2 0.5 0.5]
+    unitSquareSamples (mkJittered 2 [1,1..]) `shouldBe` [Vector2 0.5 0.5, Vector2 1 0.5, Vector2 0.5 1, Vector2 1 1]
+
 standardSamplerSpec :: Spec
-standardSamplerSpec = describe "StandardSampler" $ do
-  it "has 1 point in the center" $
-    mkStandard `shouldBe` Sampler 1 [Vector2 0.5 0.5]
+standardSamplerSpec = describe "StandardSampler" $
+  it "has 1 point in the center" $ do
+    let
+      s = mkStandard
+    numSamples s `shouldBe` 1
+    unitSquareSamples s `shouldBe` [Vector2 0.5 0.5]
+
+randomSamplerSpec :: Spec
+randomSamplerSpec = describe "RandomSampler" $ do
+  it "has given number of samples" $ do
+    numSamples (mkRandom 2 [0..]) `shouldBe` 2
+    numSamples (mkRandom 3 [0..]) `shouldBe` 3
+  it "uses given list to generate samples" $
+    unitSquareSamples (mkRandom 2 [0..4]) `shouldNotBe` unitSquareSamples (mkRandom 2 [1..5])
 
 regularSamplerSpec :: Spec
 regularSamplerSpec = describe "RegularSampler" $ do
+  it "has squared number of given argument" $ do
+    numSamples (mkRegular 2) `shouldBe` 4
+    numSamples (mkRegular 3) `shouldBe` 9
   it "distributes evenly" $
-    mkRegular 2 `shouldBe` Sampler 4 [Vector2 0.25 0.25, Vector2 0.75 0.25, Vector2 0.25 0.75, Vector2 0.75 0.75]
+    unitSquareSamples (mkRegular 2) `shouldBe` [Vector2 0.25 0.25, Vector2 0.75 0.25, Vector2 0.25 0.75, Vector2 0.75 0.75]
 
