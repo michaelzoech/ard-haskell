bae3655f884d7c319229b4367f9e8b5f46b2ed03
Michael Zoech <michi.zoech@gmail.com>
2016-11-27 22:03:37 -0800

    Extract block parsing into separate function

--- lib/ARD/Parser.hs
+++ lib/ARD/Parser.hs
@@ -50,6 +50,8 @@ data Context
   , globals :: Globals
   }
 
+type SceneParser = CharParser Context
+
 eitherFromMaybe :: Maybe a -> b -> Either b a
 eitherFromMaybe Nothing b = Left b
 eitherFromMaybe (Just a) _ = Right a
@@ -92,13 +94,14 @@ parseScene sourceName input =
       Left err -> Left $ show err
       Right context -> Right context
 
-pScene :: CharParser Context Context
+pScene :: SceneParser Context
 pScene = do
   spaces
-  manyTill ((pComment <|> pLet <|> pCamera <|> pViewPlane <|> pLight <|> pPlane <|> pSphere <|> pBox <|> pBackgroundColor <|> pRandom <|> pAmbientLight) <* spaces) (try eof)
+  many ((pComment <|> pLet <|> pCamera <|> pViewPlane <|> pLight <|> pPlane <|> pSphere <|> pBox <|> pBackgroundColor <|> pRandom <|> pAmbientLight) <* spaces)
+  eof
   getState
 
-pLet :: CharParser Context ()
+pLet :: SceneParser ()
 pLet = do
   try $ pSymbol "let"
   name <- pIdentifier
@@ -112,62 +115,59 @@ pLet = do
     spaces
     pGlobalMaterial name <|> pGlobalColor name
 
-pGlobalMaterial :: String -> CharParser Context ()
+pGlobalMaterial :: String -> SceneParser ()
 pGlobalMaterial name = do
   try $ pSymbol "material"
   material <- pMaterialBlock
   updateGlobals $ \g -> g { globalMaterials = Map.insert name material (globalMaterials g) }
 
-pGlobalColor :: String -> CharParser Context ()
+pGlobalColor :: String -> SceneParser ()
 pGlobalColor name = do
   try $ pSymbol "color"
   color <- pColorBlock
   updateGlobals $ \g -> g { globalColors = Map.insert name color (globalColors g) }
 
-pBackgroundColor :: CharParser Context ()
+pBackgroundColor :: SceneParser ()
 pBackgroundColor = do
   try $ pSymbol "backgroundColor"
   color <- pColor
   spaces
   updateState $ \c -> c { backgroundColor = Just color }
 
-pRandom :: CharParser Context ()
+pRandom :: SceneParser ()
 pRandom = do
   try $ pSymbol "random"
   seed <- pInt
   spaces
   updateState $ \c -> c { randomState = Randomize.mkRandomState seed }
 
-pAmbientLight :: CharParser Context ()
+pAmbientLight :: SceneParser ()
 pAmbientLight = do
   try $ pSymbol "ambientLight"
   light <- pLightBlock
   spaces
   updateState $ \c -> c { ambientLight = Just light }
 
-pViewPlane :: CharParser Context ()
+pViewPlane :: SceneParser ()
 pViewPlane = do
   try $ pSymbol "viewplane"
-  pBraceOpen
-  hres <- pField "horizontal" pInt
-  vres <- pField "vertical" pInt
-  pixelSize <- pField "pixelsize" pDouble
-  sampler <- pField "sampler" pSampler
-  pBraceClose
-  updateState $ \c -> c { viewPlane = Just ViewPlane.ViewPlane
-    { ViewPlane.horizontalResolution = hres
-    , ViewPlane.verticalResolution = vres
-    , ViewPlane.pixelSize = pixelSize
-    , ViewPlane.pixelSampler = sampler
+  pBlock $ do
+    hres <- pField "horizontal" pInt
+    vres <- pField "vertical" pInt
+    pixelSize <- pField "pixelsize" pDouble
+    sampler <- pField "sampler" pSampler
+    updateState $ \c -> c { viewPlane = Just ViewPlane.ViewPlane
+      { ViewPlane.horizontalResolution = hres
+      , ViewPlane.verticalResolution = vres
+      , ViewPlane.pixelSize = pixelSize
+      , ViewPlane.pixelSampler = sampler
+      }
     }
-  }
 
-pCamera :: CharParser Context ()
+pCamera :: SceneParser ()
 pCamera = do
   try $ pSymbol "camera"
-  pBraceOpen
-  ctype <- try (pSymbol "orthographic") <|> try (pSymbol "pinhole")
-  camera <- case ctype of
+  camera <- pBlock $ pString >>= \s -> case s of
     "orthographic" -> do
       eye <- pField "eye" pVector3
       lookAt <- pField "lookAt" pVector3
@@ -179,101 +179,90 @@ pCamera = do
       up <- pField "up" pVector3
       distance <- pField "distance" pDouble
       return $ Camera.mkPinhole eye lookAt up distance
-  pBraceClose
+    _ -> fail ("Unexpected camera type " ++ s)
   updateState $ \c -> c { camera = Just camera }
 
-pSphere :: CharParser Context ()
+pSphere :: SceneParser ()
 pSphere = do
   try $ pSymbol "sphere"
-  pBraceOpen
-  center <- pField "center" pVector3
-  radius <- pField "radius" pDouble
-  material <- pField "material" pMaterial
-  pBraceClose
-  let
-    sphere = World.SceneObject Sphere.Sphere
-      { Sphere.center = center
-      , Sphere.radius = radius
-      , Sphere.material = material
-      }
-  updateState $ \c -> c { sceneObjects = sceneObjects c ++ [sphere] }
+  pBlock $ do
+    center <- pField "center" pVector3
+    radius <- pField "radius" pDouble
+    material <- pField "material" pMaterial
+    let
+      sphere = World.SceneObject Sphere.Sphere
+        { Sphere.center = center
+        , Sphere.radius = radius
+        , Sphere.material = material
+        }
+    updateState $ \c -> c { sceneObjects = sceneObjects c ++ [sphere] }
 
-pPlane :: CharParser Context ()
+pPlane :: SceneParser ()
 pPlane = do
   try $ pSymbol "plane"
-  pBraceOpen
-  point <- pField "point" pVector3
-  normal <- pField "normal" pVector3
-  material <- pField "material" pMaterial
-  pBraceClose
-  let
-    plane = World.SceneObject Plane.Plane
-      { Plane.point = point
-      , Plane.normal = normal
-      , Plane.material = material
-      }
-  updateState $ \c -> c { sceneObjects = sceneObjects c ++ [plane] }
+  pBlock $ do
+    point <- pField "point" pVector3
+    normal <- pField "normal" pVector3
+    material <- pField "material" pMaterial
+    let
+      plane = World.SceneObject Plane.Plane
+        { Plane.point = point
+        , Plane.normal = normal
+        , Plane.material = material
+        }
+    updateState $ \c -> c { sceneObjects = sceneObjects c ++ [plane] }
 
-pBox :: CharParser Context ()
+pBox :: SceneParser ()
 pBox = do
   try $ pSymbol "box"
-  pBraceOpen
-  center <- pField "center" pVector3
-  Vector.Vector3 sx sy sz <- pField "size" pVector3
-  Vector.Vector3 rx ry rz <- pField "rotation" pVector3
-  material <- pField "material" pMaterial
-  pBraceClose
-  let
-    m = Matrix.rotateX rx `Matrix.mul` Matrix.rotateY ry `Matrix.mul` Matrix.rotateZ rz
-    vu = m `Matrix.transformVector` Vector.Vector3 (0.5*sx) 0 0
-    vv = m `Matrix.transformVector` Vector.Vector3 0 (0.5*sy) 0
-    vw = m `Matrix.transformVector` Vector.Vector3 0 0 (0.5*sz)
-    box = World.SceneObject Box.Box
-      { Box.center = center
-      , Box.vu = vu
-      , Box.vv = vv
-      , Box.vw = vw
-      , Box.material = material
-      }
-  updateState $ \c -> c { sceneObjects = sceneObjects c ++ [box] }
+  pBlock $ do
+    center <- pField "center" pVector3
+    Vector.Vector3 sx sy sz <- pField "size" pVector3
+    Vector.Vector3 rx ry rz <- pField "rotation" pVector3
+    material <- pField "material" pMaterial
+    let
+      m = Matrix.rotateX rx `Matrix.mul` Matrix.rotateY ry `Matrix.mul` Matrix.rotateZ rz
+      vu = m `Matrix.transformVector` Vector.Vector3 (0.5*sx) 0 0
+      vv = m `Matrix.transformVector` Vector.Vector3 0 (0.5*sy) 0
+      vw = m `Matrix.transformVector` Vector.Vector3 0 0 (0.5*sz)
+      box = World.SceneObject Box.Box
+        { Box.center = center
+        , Box.vu = vu
+        , Box.vv = vv
+        , Box.vw = vw
+        , Box.material = material
+        }
+    updateState $ \c -> c { sceneObjects = sceneObjects c ++ [box] }
 
-pMaterial :: CharParser Context Material.Material
+pMaterial :: SceneParser Material.Material
 pMaterial = pReference globalMaterials <|> pMaterialBlock
 
-pMaterialBlock :: CharParser Context Material.Material
-pMaterialBlock = do
-  try pBraceOpen
-  mtype <- try (pSymbol "matte") <|> try (pSymbol "normal") <|> try (pSymbol "phong")
-  material <- case mtype of
+pMaterialBlock :: SceneParser Material.Material
+pMaterialBlock = pBlock $
+  pString >>= \s -> case s of
     "matte" -> Material.mkMatte <$> pField "cd" pColor <*> pField "kd" pDouble <*> pField "ka" pDouble
     "normal" -> return Material.mkNormal
     "phong" -> Material.mkPhong <$> pField "cd" pColor <*> pField "kd" pDouble <*> pField "ka" pDouble <*> pField "ks" pDouble <*> pField "exp" pDouble
-  pBraceClose
-  return material
+    _ -> fail ("Unexpected material type " ++ s)
 
-pLight :: CharParser Context ()
+pLight :: SceneParser ()
 pLight = do
   try $ pSymbol "light"
   light <- pLightBlock
   updateState $ \c -> c{ lights = lights c ++ [light] }
 
-pLightBlock :: CharParser Context Light.Light
-pLightBlock = do
-  pBraceOpen
-  ltype <- try (pSymbol "ambient") <|> try (pSymbol "ambientOccluder") <|> try (pSymbol "directional") <|> try (pSymbol "point")
-  light <- case ltype of
+pLightBlock :: SceneParser Light.Light
+pLightBlock = pBlock $
+  pString >>= \s -> case s of
     "ambient" -> Light.mkAmbient <$> pField "color" pColor <*> pField "ls" pDouble
     "ambientOccluder" -> Light.mkAmbientOccluder <$> pField "color" pColor <*> pField "ls" pDouble <*> pField "minColor" pColor <*> pField "sampler" pSampler
     "directional" -> Light.mkDirectional <$> pField "invertDirection" pVector3 <*> pField "color" pColor <*> pField "ls" pDouble
     "point" -> Light.mkPoint <$> pField "location" pVector3 <*> pField "color" pColor <*> pField "ls" pDouble
-  pBraceClose
-  return light
+    _ -> fail ("Unexpected light type " ++ s)
 
-pSampler :: CharParser Context Sampler.Sampler
-pSampler = do
-  pBraceOpen
-  stype <- try (pSymbol "jittered") <|> try (pSymbol "random") <|> try (pSymbol "regular") <|> try (pSymbol "standard")
-  sampler <- case stype of
+pSampler :: SceneParser Sampler.Sampler
+pSampler = pBlock $
+  pString >>= \s -> case s of
     "jittered" -> do
       axis <- pField "axis" pInt
       randomizeFromContext (Sampler.mkJittered axis)
@@ -284,22 +273,21 @@ pSampler = do
       axis <- pField "axis" pInt
       randomizeFromContext (Sampler.mkRegular axis)
     "standard" -> randomizeFromContext Sampler.mkStandard
-  pBraceClose
-  return sampler
+    _ -> fail ("Unexpected sampler type " ++ s)
 
-pVector2 :: CharParser Context Vector.Vector2
+pVector2 :: SceneParser Vector.Vector2
 pVector2 = Vector.Vector2 <$> (spaces *> pDouble) <*> (spaces1 *> pDouble)
 
-pVector3 :: CharParser Context Vector.Vector3
+pVector3 :: SceneParser Vector.Vector3
 pVector3 = Vector.Vector3 <$> (spaces *> pDouble) <*> (spaces1 *> pDouble) <*> (spaces1 *> pDouble)
 
-pColor :: CharParser Context Color.Color
+pColor :: SceneParser Color.Color
 pColor =  pReference globalColors <|> pColorBlock
 
-pColorBlock :: CharParser Context Color.Color
+pColorBlock :: SceneParser Color.Color
 pColorBlock = Color.RGB <$> (spaces *> pDouble) <*> (spaces1 *> pDouble) <*> (spaces1 *> pDouble)
 
-pReference :: (Globals -> Map.Map String a) -> CharParser Context a
+pReference :: (Globals -> Map.Map String a) -> SceneParser a
 pReference f = do
   name <- try pIdentifier
   context <- getState
@@ -307,55 +295,55 @@ pReference f = do
     Just m -> return m
     _ -> fail ("Reference with name '" ++ name ++ "' but global not found")
 
-pField :: String -> CharParser Context a -> CharParser Context a
+pField :: String -> SceneParser a -> SceneParser a
 pField key valueParser = pSymbol key *> valueParser <* notFollowedBy alphaNum <* spaces
 
-pSymbol :: String -> CharParser Context String
+pSymbol :: String -> SceneParser String
 pSymbol symbol = string symbol <* notFollowedBy alphaNum <* spaces
 
-pIdentifier :: CharParser Context String
+pString :: SceneParser String
+pString = pIdentifier <* spaces
+
+pIdentifier :: SceneParser String
 pIdentifier = do
   first <- letter <|> char '_'
   rest <- many (alphaNum <|> char '_')
   return (first:rest)
 
-pComment :: CharParser Context ()
+pComment :: SceneParser ()
 pComment = do
   char '#'
   manyTill (noneOf "\r\n") (void (oneOf "\r\n") <|> eof)
   many (oneOf "\r\n")
   return ()
 
-pDouble :: CharParser Context Double
+pDouble :: SceneParser Double
 pDouble = do
   s <- getInput
   case readSigned readFloat s of
     [(n, s')] -> n <$ setInput s'
     _         -> empty
 
-pInt :: CharParser Context Int
+pInt :: SceneParser Int
 pInt = do
   s <- getInput
   case readSigned readDec s of
     [(n, s')] -> n <$ setInput s'
     _         -> empty
 
-pBraceOpen :: CharParser Context ()
-pBraceOpen = char '{' >> spaces
+pBlock :: SceneParser a -> SceneParser a
+pBlock = between (char '{' >> spaces) (char '}' >> spaces)
 
-pBraceClose :: CharParser Context ()
-pBraceClose = char '}' >> spaces
-
-spaces1 :: CharParser Context String
+spaces1 :: SceneParser String
 spaces1 = many1 space
 
-randomizeFromContext :: Randomize.Randomize a -> CharParser Context a
+randomizeFromContext :: Randomize.Randomize a -> SceneParser a
 randomizeFromContext f = do
   context <- getState
   let (result, r') = Randomize.runRandomized f (randomState context)
   updateState $ \c -> c { randomState = r' }
   return result
 
-updateGlobals :: (Globals -> Globals) -> CharParser Context ()
+updateGlobals :: (Globals -> Globals) -> SceneParser ()
 updateGlobals f = updateState $ \c -> c { globals = f (globals c) }
 
