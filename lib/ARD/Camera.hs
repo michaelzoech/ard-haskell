1e087d2e077c9a2ba6fb89dbe82c651b801b47e4
Michael Zoech <michi.zoech@gmail.com>
2017-03-16 21:09:54 -0700

    Make Camera type hold data only

--- lib/ARD/Camera.hs
+++ lib/ARD/Camera.hs
@@ -1,55 +1,48 @@
 module ARD.Camera
-  ( Camera(..)
+  ( Camera
   , mkOrthographic
   , mkPinhole
+  , generateRay
+  , uvw
   ) where
 
 import ARD.Ray
 import ARD.Vector
 
 data Camera
-  = Camera
-  { eye :: Vector3
-  , lookAt :: Vector3
-  , up :: Vector3
-  , uvw :: (Vector3, Vector3, Vector3)
-  , generateRay :: Vector2 -> Ray
-  }
+  = Orthographic Vector3 Vector3 Vector3 (Vector3, Vector3, Vector3)
+  | Pinhole Vector3 Double (Vector3, Vector3, Vector3)
 
 mkOrthographic :: Vector3 -> Vector3 -> Vector3 -> Camera
 mkOrthographic eye lookAt up =
   let
     uvw@(u,v,w) = calculateUVW eye lookAt up
   in
-    Camera
-    { eye = eye
-    , lookAt = lookAt
-    , up = normalize up
-    , uvw = uvw
-    , generateRay = \(Vector2 x y) ->
-        let
-          origin = eye + (u `mul` x) + (v `mul` y)
-          direction = lookAt - eye
-        in
-          Ray origin direction
-    }
+    Orthographic eye lookAt (lookAt-eye) uvw
 
 mkPinhole :: Vector3 -> Vector3 -> Vector3 -> Double -> Camera
 mkPinhole eye lookAt up distance =
   let
     uvw@(u,v,w) = calculateUVW eye lookAt up
   in
-    Camera
-    { eye = eye
-    , lookAt = lookAt
-    , up = up
-    , uvw = uvw
-    , generateRay = \(Vector2 x y) ->
-        let
-          direction = (u `mul` x) + (v `mul` y) - (w `mul` distance)
-        in
-          Ray eye (normalize direction)
-    }
+    Pinhole eye distance uvw
+
+generateRay :: Camera -> Vector2 -> Ray
+generateRay (Orthographic eye lookAt direction (u,v,_)) (Vector2 x y) =
+  let
+    origin = eye + (u `mul` x) + (v `mul` y)
+    direction = lookAt - eye
+  in
+    Ray origin direction
+generateRay (Pinhole eye distance (u,v,w)) (Vector2 x y) =
+  let
+    direction = (u `mul` x) + (v `mul` y) - (w `mul` distance)
+  in
+    Ray eye (normalize direction)
+
+uvw :: Camera -> (Vector3, Vector3, Vector3)
+uvw (Orthographic _ _ _ uvw) = uvw
+uvw (Pinhole _ _ uvw) = uvw
 
 calculateUVW :: Vector3 -> Vector3 -> Vector3 -> (Vector3, Vector3, Vector3)
 calculateUVW eye lookAt up =
